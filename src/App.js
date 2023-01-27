06477a4ce0b973b9e79d824ffafa94b7ff151be8
mah-deve <martart.lune@gmail.com>
2023-01-27 20:39:41 -0300

    meuapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+  const [peso, setPeso] = useState(''); 
+  const [altura, setAltura] = useState(''); 
 
-export default App;
+  const[mensagem, setMensagem] = useState('');
+
+  function calIMC() {
+    const alt = altura / 100;
+    const imc = peso / (alt + alt);
+
+    if(imc < 18.5) {
+      setMensagem("Você está abaixo do peso! Seu IMC: " + imc.toFixed(2))
+    }else if(imc >= 18.5 && imc < 24.9){
+      setMensagem("Você está no peso normal! Seu IMC: " + imc.toFixed(2))
+    }else if(imc >= 24.9 &&  imc < 34.9){
+      setMensagem("Você está na Obesidade grau 1! Seu IMC: " + imc.toFixed(2))
+    }else if(imc >= 34.9 && imc < 39.9){
+      setMensagem("Você está na obesidade grau 2! Seu IMC: " + imc.toFixed(2))
+    }else if(imc > 40){
+      setMensagem("Cuidado, está na obsesidade grau 3! Seu IMC: " + imc.toFixed(2))
+    }
+  }
+
+  return (
+    <div className="app">
+      <h1>Calculadora IMC</h1>
+      <span>Calcular IMC</span>
+    
+      <div className="input">
+        <input type="text" placeholder="Peso (KG)"
+        value={peso}
+        onChange={(evento) => setPeso(evento.target.value)} //Saber o que o usuario está digitando. 
+        ></input>
+        
+        
+        <input type="text" placeholder="Altura (CM) "
+        value={altura}
+        onChange={(evento) => setAltura(evento.target.value)}></input>
+        
+      
+        <button onClick={calIMC}>
+          Calcular
+        </button>
+      </div>
+    
+      <h2>{mensagem}</h2>
+
+    </div>
+  )
+}
